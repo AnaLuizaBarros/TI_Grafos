4f7b354273fff0b6ba2aa5655e9be98e38f0770c
AnaLuizaBarros <analuizagbarros@gmail.com>
2020-11-24 11:41:52 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TI_Grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t Alunos: " +
+              "\n Ana Luiza Gonçalves Lourenço Barros" +
+              "\n Douglas Barbosa da Silva" +
+              "\n Jonathan William de Paiva" +
+              "\n Lucas Gomes Oliveira" +
+              "\n Victor Henrique de Souza Oliveira \n");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
